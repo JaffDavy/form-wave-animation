f037b352ab7769181599f9894cfb7dc07acab393
Jaffdavy <jaffdavy100@gmail.com>
2024-01-10 12:35:59 +0100

    lint errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const label = document.querySelectorAll('.form-control label')
 
 label.forEach(label => {
   label.innerHTML = label.innerText
-  .split('')
-  .map((letter, idx) =>`<span style="transition-delay:${idx * 50}ms">${letter}</span>` )
-  .join('')
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
 })
